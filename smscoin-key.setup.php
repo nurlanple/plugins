1ce9f54037c49d770b05334be1e470c7f61732cf
nurlanple <kenzhik-studio@mail.ru>
2014-04-24 10:11:01 +0600

    Загрузка основных файлов сборки
    Загрузка основных файлов сборки

---
+++ smscoin-key.setup.php
@@ -0,0 +1,25 @@
+<?php
+/* ==================== 
+[BEGIN_COT_EXT]
+Code=smscoin-key
+Name=SmsCoin hidden content
+Description= Данный плагин скрывает любую часть вашего сайта. Чтобы посмотреть скрытую часть сайта, пользователь должен ввести код который получить после отправки SMS на короткий номер системы. Если пользователь еще не отправил SMS,  тогда ему будет предложена инструкция по оплате через SMS.
+Notes= Version= 0.0.2
+Date=
+Author=Nurlan Kenzhegulov
+Copyright=Copyright (c) webm.kz 
+Auth_guests=R 
+Lock_guests=W12345A 
+Auth_members=RW 
+Lock_members=12345A 
+[END_COT_EXT]
+
+[BEGIN_COT_EXT_CONFIG]
+lang=02:select:choose language,english,russian,estonian,ukrainian,romanian,belarusian,french,spanish,german,hebrew,lithuanian,latvian,polish,portuguese,portuguese_br,czech,slovak,arabic,turkish,swedish,greek::Language
+key_id=01:string:::Sms key ID
+[END_COT_EXT_CONFIG]
+==================== */  
+
+defined('COT_CODE') or die('Wrong URL');
+
+?>
